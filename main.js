eed7fc1fb9e937ae5ac4f26dfe56338fc3ff1857
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-20 18:44:43 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+function init(){
+		if (document.getElementById("list").style.display === "block"){
+			document.getElementById("list").style.display = "none";
+		}
+		else{
+			document.getElementById("list").style.display = "block";
+		}
+	}
