a343498c95fb9183de3d38f3726b8ed0d9f10ee0
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-29 12:36:31 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 function init()
 {
-		if (document.getElementById("list").style.display === "block")
-		{
-			document.getElementById("list").style.display = "none";
-		}
-		else
-		{
-			document.getElementById("list").style.display = "block";
-		}
+	// Made for the dropdown(to appear only when clicked on, not otherwise.
+	// I think there might be a way to do this using pure CSS but I don't really know.
+	
+	if (document.getElementById("list").style.display === "block")
+	{
+		document.getElementById("list").style.display = "none";
+	}
+	else
+	{
+		document.getElementById("list").style.display = "block";
+	}
 }
