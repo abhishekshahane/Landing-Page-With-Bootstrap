38e51d5643ebc0f36b053aad2aba18b8238fdca8
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-10-29 12:33:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+/*
+Note: This file, index.php was only made solely for deploying this
+website to Heroku(for reference). Note that this file is NOT part of the actual
+source code for this website.
+*/
 
 include_once("index.html");
 
